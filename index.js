f36058f40da19fee96c691e6fbcd2ce410482c81
Francesca <fraanyrodriguez@gmail.com>
2022-04-21 22:58:42 -0300

    primera implementacion de tablero

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class MemoTest {
     this.grilla = {};
     this.primeraPosicion;
     this.segundaPosicion;
+    this.primeraCarta;
+    this.segundaCarta;
     this.seguirJugando = true;
     this.puntos = 0;
     this.posicionesAcertadas = [];
@@ -14,122 +16,72 @@ class MemoTest {
     });
 
     for (let i = 0; i < this.imagenes.length; i++) {
-      this.grilla[i] = this.imagenes[i];
+      this.grilla[i.toString()] = this.imagenes[i];
     }
     console.log(this.grilla);
   }
+  mostrarCarta(posicion) {
+    document.getElementById(posicion).innerHTML = this.grilla[posicion];
+  }
+  ocultarCarta(posicion) {
+    document.getElementById(posicion).innerHTML = "";
+  }
+  jugada(posicion) {
+    if (this.posicionesAcertadas.includes(posicion)) return;
 
-  cargarPrimeraPosicion() {
-    this.primeraPosicion = parseInt(this.inputPrimeraPosicion.value);
-    if (!this.esPosicionValida(this.primeraPosicion)) {
-      const mensaje = !this.esNumero(this.primeraPosicion)
-        ? "Eso no es un número. Ingresá un número"
-        : "La posición no es válida. Ingresá un número del 0 al 15";
-      this.primerMensaje.innerHTML = mensaje;
+    this.mostrarCarta(posicion);
+    console.log(posicion);
+    console.log(this.grilla[posicion]);
+    if (!this.primeraCarta) {
+      this.primeraPosicion = posicion;
+      this.primeraCarta = this.grilla[posicion];
     } else {
-      this.primerMensaje.innerHTML =
-        "En la posición que ingresaste está la carta " +
-        this.grilla[this.primeraPosicion];
-    }
-  }
+      if (posicion === this.primeraPosicion) return;
 
-  cargarSegundaPosicion() {
-    this.segundaPosicion = parseInt(this.inputSegundaPosicion.value);
-    if (!this.esPosicionValida(this.segundaPosicion)) {
-      const mensaje = !this.esNumero(this.segundaPosicion)
-        ? "Eso no es un número. Ingresá un número"
-        : "La posición no es válida. Ingresá un número del 0 al 15";
-      this.segundoMensaje.innerHTML = mensaje;
-    } else if (this.primeraPosicion === this.segundaPosicion) {
-      this.segundoMensaje.innerHTML =
-        "La posición no es válida. Ingresá un número distinto a la primera posición";
-    } else {
-      this.segundoMensaje.innerHTML =
-        "En la posición que ingresaste está la carta " +
-        this.grilla[this.segundaPosicion];
-    }
-    this.comprobarSiFueIngresada();
-  }
-
-  esNumero(dato) {
-    return !isNaN(dato);
-  }
-
-  esPosicionValida(dato) {
-    if (this.esNumero(dato)) {
-      if (dato >= 0 && dato <= 15) {
-        return true;
-      }
+      this.segundaPosicion = posicion;
+      this.segundaCarta = this.grilla[posicion];
+      this.compararImagenes();
     }
   }
 
   compararImagenes() {
-    if (
-      this.grilla[this.primeraPosicion] === this.grilla[this.segundaPosicion] &&
-      this.primeraPosicion != this.segundaPosicion &&
-      !this.comprobarSiFueIngresada()
-    ) {
-      this.tercerMensaje.innerHTML = "¡Felicitaciones! Acertaste";
+    if (this.primeraCarta === this.segundaCarta) {
       this.contarPuntos();
       this.guardarPosicionesAcertadas();
-    } else if (
-      this.comprobarSiFueIngresada() ||
-      this.primeraPosicion === this.segundaPosicion
-    ) {
-      this.segundoMensaje.innerHTML = "Ya ingresaste esa posición";
     } else {
-      this.tercerMensaje.innerHTML = "No acertaste:(";
+      setTimeout(() => {
+        this.ocultarCarta(this.primeraPosicion);
+        this.ocultarCarta(this.segundaPosicion);
+        this.primeraPosicion = null;
+        this.segundaPosicion = null;
+        this.primeraCarta = null;
+        this.segundaCarta = null;
+      }, 700);
     }
-    this.seguirJugandoBtn.style.display = "block";
   }
 
   contarPuntos() {
     this.puntos++;
     this.mostrarPuntos.innerHTML = "Puntos: " + this.puntos;
+    if (this.puntos === 8) {
+      document.getElementById("tablero").innerHTML =
+        "<h1>Felicidades!! Ganaste :D </h1>";
+    }
   }
   guardarPosicionesAcertadas() {
     this.posicionesAcertadas.push(this.primeraPosicion);
     this.posicionesAcertadas.push(this.segundaPosicion);
   }
 
-  comprobarSiFueIngresada() {
-    if (
-      this.posicionesAcertadas.includes(this.primeraPosicion) ||
-      this.posicionesAcertadas.includes(this.segundaPosicion)
-    ) {
-      return true;
-    }
-  }
   jugarTurno() {}
 
   comenzarJuego() {
-    this.inputPrimeraPosicion = document.getElementById("primeraPosicion");
-    this.inputSegundaPosicion = document.getElementById("segundaPosicion");
-    this.primeraPosBtn = document.getElementById("primeraPosBtn");
-    this.segundaPosBtn = document.getElementById("segundaPosBtn");
-    this.primerMensaje = document.getElementById("mensaje1");
-    this.segundoMensaje = document.getElementById("mensaje2");
-    this.tercerMensaje = document.getElementById("mensaje3");
-    this.seguirJugandoBtn = document.getElementById("seguirJugandoBtn");
     this.mostrarPuntos = document.getElementById("puntos");
-
-    this.primeraPosBtn.addEventListener("click", () => {
-      this.cargarPrimeraPosicion();
-    });
-
-    this.segundaPosBtn.addEventListener("click", () => {
-      this.cargarSegundaPosicion();
-      this.compararImagenes();
-    });
-    this.seguirJugandoBtn.addEventListener("click", () => {
-      this.seguirJugandoBtn.style.display = "none";
-      this.primerMensaje.innerHTML = "";
-      this.segundoMensaje.innerHTML = "";
-      this.tercerMensaje.innerHTML = "";
-      this.inputPrimeraPosicion.value = "";
-      this.inputSegundaPosicion.value = "";
-      console.log(this.posicionesAcertadas);
-    });
+    for (let i = 0; i <= 15; i++) {
+      document.getElementById(i.toString()).addEventListener("click", () => {
+        this.jugada(i.toString());
+      });
+    }
   }
 }
 
